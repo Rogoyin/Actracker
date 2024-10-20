61732d87fbb4192ebdfc18740a58bf381d9c805c
Rogoyin <patricioe.nogueroles@gmail.com>
2024-10-20 18:24:02 -0300

    Inicio de armado del programa final

---
+++ BetActracker.py
@@ -0,0 +1,16 @@
+##################
+### BIBLIOTECAS ##
+##################
+
+import pandas as pd
+import datetime as dt 
+import time  
+import tkinter as tk 
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Alignment, Border, Side
+import pygame
+import random
+import sys
+from pathlib import Path
+import os
+
